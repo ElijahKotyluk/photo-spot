64de63dfb8faf56095e2cfb370d187486fa25d90
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-04 14:30:43 -0700

    Begin creating app layout:  - Install vuetify  - Create layout directory and vue file  - Add vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,10 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -11,9 +15,13 @@ window.Vue = require('vue');
 import store from './store';
 import HomePage from './components/HomePage';
 
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+
 Vue.component('home-page', HomePage);
 
 const app = new Vue({
     el: '#app',
+    router,
     store
 });
